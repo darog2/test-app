4c381bedb290983093bdec30f60839dfc4162b14
yourlastmemory <shadow00hunter00@gmail.com>
2021-04-03 11:12:40 +0300

    shaker sort

--- test-app/src/com/dungeon/test/Main.java
+++ test-app/src/com/dungeon/test/Main.java
@@ -2,27 +2,41 @@ package com.dungeon.test;
 
 import com.dungeon.common.CommonInputUtils;
 
+
 import java.util.Arrays;
+
 
 public class Main {
     public static void main( String[] args) {
 
-        int [] mas =CommonInputUtils.getInstance().createRandomIntArray(8,2,22);
-
-        boolean isSorted = false;
-        int buf;
-        while(!isSorted) {
+        int[] mas = CommonInputUtils.getInstance().createRandomIntArray(10, 2, 22);
+//        int[] mas = {17, 14, 8, 6, 2, 4, 12, 4, 8, 5};
+        System.out.println(Arrays.toString(mas));
+        boolean isSorted;
+        int cnt=0;
+        do {
+            cnt++;
             isSorted = true;
-            for (int i = 0; i < mas.length-1; i++) {
-                if(mas[i] > mas[i+1]){
+            for (int i = 0; i < mas.length - 1; i++) {
+                if (mas[0 + i] > mas[0 + 1 + i]) {
                     isSorted = false;
-
-                    buf = mas[i];
-                    mas[i] = mas[i+1];
-                    mas[i+1] = buf;
+                    swap(mas, i, i + 1);
+                }
+                if (mas[mas.length - 1-i] < mas[mas.length - 2-i]) {
+                    isSorted = false;
+                    swap(mas, mas.length - 1-i, mas.length - 2-i);
                 }
             }
-        }
+        } while (!isSorted);
         System.out.println(Arrays.toString(mas));
+        System.out.println(cnt);
+    }
+
+    public static void swap(int[] mas, int i1, int i2) {
+        int buf;
+
+        buf = mas[i1];
+        mas[i1] = mas[i2];
+        mas[i2] = buf;
     }
 }

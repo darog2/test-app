dad1acf9d55815ae1dc42e407f7df713368a074f
yourlastmemory <shadow00hunter00@gmail.com>
2021-04-07 19:49:01 +0300

    WIP: formatting receipt

--- test-app/src/com/dungeon/market/Main.java
+++ test-app/src/com/dungeon/market/Main.java
@@ -4,8 +4,8 @@ import com.dungeon.market.model.Cashier;
 import com.dungeon.market.model.Market;
 import com.dungeon.market.model.Product;
 import com.dungeon.market.model.Receipt;
+import com.dungeon.market.service.ReceiptService;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
@@ -14,10 +14,10 @@ public class Main {
         Cashier cashier = new Cashier();
         cashier.setName("dasha");
         cashier.setLastName("Pupkina");
-        Cashier cashier1 = new Cashier("ivan", "Govnow");
+        Cashier cashier1 = new Cashier("ivan", "Ivanov");
         Product product = new Product("масло подсолнечное 1 литр", 28);
         Product product1 = new Product();
-        product1.setName("Шокол. Рошен \"Говно Пети\"");
+        product1.setName("Шокол. Рошен \"и\"");
         product1.setPrice(35);
         Market market=new Market("ATB",5);
         Receipt receipt=new Receipt();
@@ -26,15 +26,7 @@ public class Main {
         Map<Product, Integer> products = receipt.getProducts();
         products.put(product,5);
         receipt.getProducts().put(product1,2);
-        double totalPrice = 0;
-
-        String date = new SimpleDateFormat("dd.MM.yyyy hh:mm").format(receipt.getDate());
-        System.out.println(market.getName());
-        System.out.println(date);
-        for (Map.Entry<Product, Integer> entry : receipt.getProducts().entrySet()) {
-            totalPrice= totalPrice + entry.getKey().getPrice()*entry.getValue();
-            System.out.println(entry.getKey().getName()+"       "+ entry.getKey().getPrice()+" x "+entry.getValue());
-        }
-        System.out.println(("Total price:  ") + totalPrice);
+        ReceiptService receiptService=new ReceiptService();
+        receiptService.printReceipt(market,receipt, 3);
     }
 }
---
+++ test-app/src/com/dungeon/market/service/ReceiptService.java
@@ -0,0 +1,44 @@
+package com.dungeon.market.service;
+
+import com.dungeon.market.model.Cashier;
+import com.dungeon.market.model.Market;
+import com.dungeon.market.model.Product;
+import com.dungeon.market.model.Receipt;
+
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+public class ReceiptService {
+    public static final String MARKET_NAME_FORMAT = "|%25.25s%25s|%n";
+    public static final String HORIZONTAL_BORDER = "\n____________________________________________________";
+    public static final String EMPTY = "";
+    public static final String EMPTY_LINE = "|                                                  |";
+    public static final String ROW_NUMBER_FORMAT = "|Касса  :    %-38d|%n";
+    public static final String CASHIER_FORMAT = "|Кассир :    %-38.38s|%n";
+    public static final String FULLNAME_FORMAT = "%s %s";
+    public static final String TIME_FORMAT = "|Время  :    %-38s|%n";
+
+    public void printReceipt(Market market, Receipt receipt, int rowNumber){
+        double totalPrice = 0;
+
+        String date = new SimpleDateFormat("dd.MM.yyyy hh:mm").format(receipt.getDate());
+        System.out.println(HORIZONTAL_BORDER);
+        System.out.printf(MARKET_NAME_FORMAT,market.getName(),EMPTY);
+        System.out.println(EMPTY_LINE);
+        System.out.printf(ROW_NUMBER_FORMAT, rowNumber);
+        System.out.printf(CASHIER_FORMAT, prepareFullName(receipt.getCashier()));
+        System.out.printf(TIME_FORMAT, date);
+        System.out.println();
+        System.out.println("              Товары");
+        System.out.println();
+        for (Map.Entry<Product, Integer> entry : receipt.getProducts().entrySet()) {
+            totalPrice= totalPrice + entry.getKey().getPrice()*entry.getValue();
+            System.out.println(entry.getKey().getName()+"       "+ entry.getKey().getPrice()+" x "+entry.getValue());
+        }
+        System.out.println(HORIZONTAL_BORDER);
+        System.out.println(("Total price:  ") + totalPrice);
+    }
+    public String prepareFullName(Cashier cashier){
+        return String.format(FULLNAME_FORMAT, cashier.getName(),cashier.getLastName());
+    }
+}

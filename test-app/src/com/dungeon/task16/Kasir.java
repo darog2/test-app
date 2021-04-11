a21b122f0fb21d37fb766e946037e1b1f195d115
Саня верни сотку <darog2>
2021-04-11 09:38:11 +0300

    test sorted

---
+++ test-app/src/com/dungeon/task16/Kasir.java
@@ -0,0 +1,23 @@
+package com.dungeon.task16;
+
+public class Kasir {
+    protected String name;
+    protected String lastName;
+
+    public String getName(String dasha) {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName(String kurvna) {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
---
+++ test-app/src/com/dungeon/task16/Main.java
@@ -0,0 +1,11 @@
+package com.dungeon.task16;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("_____________________ATB");
+        Kasir kasir = new Kasir();
+        kasir.getLastName("Kurvna");
+        kasir.getName("Dasha");
+//        System.;
+    }
+}

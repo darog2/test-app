e4e7c576cd4fcc57f8c6caa64b2a8b417d373d4d
Саня верни сотку <darog2>
2021-04-11 09:36:50 +0300

    test sorted

--- test-app/src/com/dungeon/test/Main.java
+++ test-app/src/com/dungeon/test/Main.java
@@ -7,22 +7,25 @@ import java.util.Arrays;
 public class Main {
     public static void main( String[] args) {
 
-        int [] mas =CommonInputUtils.getInstance().createRandomIntArray(8,2,22);
-
-        boolean isSorted = false;
-        int buf;
-        while(!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < mas.length-1; i++) {
-                if(mas[i] > mas[i+1]){
-                    isSorted = false;
-
-                    buf = mas[i];
-                    mas[i] = mas[i+1];
-                    mas[i+1] = buf;
-                }
-            }
-        }
-        System.out.println(Arrays.toString(mas));
+//        int [] mas =CommonInputUtils.getInstance().createRandomIntArray(8,2,22);
+//
+//        boolean isSorted = false;
+//        int buf;
+//        while(!isSorted) {
+//            isSorted = true;
+//            for (int i = 0; i < mas.length-1; i++) {
+//                if(mas[i] > mas[i+1]){
+//                    isSorted = false;
+//
+//                    buf = mas[i];
+//                    mas[i] = mas[i+1];
+//                    mas[i+1] = buf;
+//                }
+//            }
+//        }
+//        System.out.println(Arrays.toString(mas));
+//    }
+        PupAge pupAge = new PupAge();
+        pupAge.pupAge();
     }
 }
---
+++ test-app/src/com/dungeon/test/PupAge.java
@@ -0,0 +1,11 @@
+package com.dungeon.test;
+
+public class PupAge {
+    public void pupAge(){
+        int age = 0;
+        age=age+7;
+        System.out.println("dog age"+age);
+
+    }
+
+}
